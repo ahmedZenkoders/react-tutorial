82fe91bf33b2eb8da359c3290d0030ccda0b7832
AhmedSiddiqui <ahmed.zenkoders@gmail.com>
2024-06-06 09:48:47 -0700

    modified code

--- src/pages/NewMeetups.jsx
+++ src/pages/NewMeetups.jsx
@@ -1,5 +1,21 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 function NewMeetupPage() {
-  return <div>New Meetup Page</div>;
+  function addMeetupHandler(meetupData) {
+    fetch("https://react-app-a9c1a-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
